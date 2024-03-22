1d636af494aadc58de3ef2d7c05e9c5d00d7ef92
trngkhang <ynguyenkhangdev@gmail.com>
2024-03-22 21:48:42 +0700

    feat: connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,21 @@
 import express from "express";
+import mongoose from "mongoose";
+import "dotenv/config";
+
+const DBcon = process.env.DB_CON;
+const port = process.env.PORT;
+
+mongoose
+  .connect(DBcon)
+  .then(() => {
+    console.log("MongoDb is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("server is running on port 3000");
+app.listen(port, () => {
+  console.log(`server is running on port: ${port}`);
 });
