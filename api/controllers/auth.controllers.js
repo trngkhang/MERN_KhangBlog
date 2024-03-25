710424eeb668608df7727cd65a3646e65d485ca8
trngkhang <ynguyenkhangdev@gmail.com>
2024-03-25 11:53:01 +0700

    feat: create sign in api route

--- api/controllers/auth.controllers.js
+++ api/controllers/auth.controllers.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -30,5 +31,40 @@ export const signup = async (req, res, next) => {
       //error handle mac dinh
       return next(error);
     }
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  console.log(req.body);
+  if (!email || !password || email === "" || password === "") {
+    console.log("1");
+    return next(errorHandler(400, "All fields are required"));
+  }
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found"));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+
+    if (!validPassword) {
+      return next(errorHandler(400, "Invalid passwword"));
+    }
+    //tao token sau khi xac thuc thanh cong voiws id
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    //lai bo password bang destructuring
+    //lay ra password va dat ten la pass, sudung ...rest de lay ra tatca cac thuoc tinh con lai
+    //thuoc tinh _doc la du lieu tho(raw) cua mongoose docudent
+    const { password: pass, ...rest } = validUser._doc;
+    return res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .json(rest);
+  } catch (error) {
+    console.log(7);
+    next(error);
   }
 };
