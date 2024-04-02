937f300533e11bfaa4748d7bf0c089b7bd54d62e
trngkhang <ynguyenkhangdev@gmail.com>
2024-04-02 22:46:15 +0700

    feat: complete sider of dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,5 +1,29 @@
-export default function Dashboard(){
-    return (
-        <div>Dashboard</div>
-    )
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import DashSideBard from "../components/DashSideBard";
+import DashProfile from "../components/DashProfile";
+
+export default function Dashboard() {
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  //xem su tahy doi tu thanh search cua trinh duyet
+  //khi hien Content con
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFormUrl = urlParams.get("tab");
+    if (tabFormUrl) {
+      setTab(tabFormUrl);
+    }
+  }, [location.search]);
+  return (
+    <div className="min-h-screen flex flex-col md:flex-row">
+      <div className="md:w-56">
+        {/* DashSidebard */}
+        <DashSideBard />
+      </div>
+      {/* UseEffect  */}
+      {/* Profile... */}
+      {tab === "profile" && <DashProfile />}
+    </div>
+  );
 }
