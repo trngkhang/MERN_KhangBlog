fc56a058a166a75c46ae43656001ba849ed5866e
trngkhang <ynguyenkhangdev@gmail.com>
2024-04-04 23:46:58 +0700

    feat: add user image upload functionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,20 +1,106 @@
 import { useSelector } from "react-redux";
-import { Button, Label, TextInput } from "flowbite-react";
+import { Alert, Button, Label, TextInput } from "flowbite-react";
+import { useState, useRef, useEffect } from "react";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 export default function DashProfile() {
   const { currentUser } = useSelector((state) => state.user);
+  const [imageFile, setImageFile] = useState(null);
+  const [imageFileUrl, setImageFileUrl] = useState(null);
+  const [imageFileUpLoadProgress, setImageFileUploadProgress] = useState(null);
+  const [imageFileUploadError, setImageFileUploadError] = useState(null);
+  const filePickerRef = useRef();
+  console.log(imageFileUpLoadProgress, imageFileUploadError);
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setImageFile(file);
+      setImageFileUrl(URL.createObjectURL(file));
+    }
+  };
+  useEffect(() => {
+    if (imageFile) {
+      uploadImage();
+    }
+  }, [imageFile]);
+  const uploadImage = async () => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + imageFile.name;
+    const storageRef = ref(storage, fileName);
+    const upLoadTask = uploadBytesResumable(storageRef, imageFile);
+    upLoadTask.on(
+      "state_changed",
+      (snapShot) => {
+        const progress =
+          (snapShot.bytesTransferred / snapShot.totalBytes) * 100;
+        setImageFileUploadProgress(progress.toFixed(0));
+      },
+      (error) => {
+        setImageFileUploadError(
+          "could not upload image (File must be less than 2MB)"
+        );
+        setImageFile(null);
+        setImageFileUrl(null);
+        setImageFileUploadProgress(null);
+      }
+    ),
+      () => {
+        getDownloadURL(upLoadTask.snapshot.ref).then((downloadUrl) => {
+          setImageFileUrl(downloadUrl);
+        });
+      };
+  };
   return (
-    <div className="w-full p-4 mb-5">
+    <div className="w-full p-4 mb-5 max-w-xl mx-auto">
       <h1 className="text-center my-6 font-semibold text-3xl">Profile</h1>
       <form className="flex flex-col">
-        <div className="self self-center w-32 h-32 cursor-pointer shadow-md rounded-full">
+        <input
+          type="file"
+          accept="image/*"
+          onChange={handleImageChange}
+          ref={filePickerRef}
+          hidden
+        />
+        <div
+          className="relative self self-center w-32 h-32 cursor-pointer shadow-md rounded-full"
+          onClick={() => filePickerRef.current.click()}
+        >
+          {imageFileUpLoadProgress && (
+            <CircularProgressbar
+              className=" absolute w-full h-full"
+              strokeWidth="4"
+              styles={buildStyles({
+                pathColor: "#00bcfc",
+              })}
+              value={imageFileUpLoadProgress}
+              maxValue={100}
+              text={`${imageFileUpLoadProgress}%`}
+            />
+          )}
           <img
-            className="w-full h-full rounded-full border-8 border-[lightgray]"
-            src={currentUser.profilePicture}
+            className={`w-full h-full rounded-full border-8 border-[lightgray] ${
+              imageFileUpLoadProgress &&
+              imageFileUpLoadProgress < 100 &&
+              "opacity-60"
+            }`}
+            src={imageFileUrl || currentUser.profilePicture}
             alt="userImg"
           />
         </div>
         <div className="mt-4 flex flex-col gap-2">
+          <div>
+            {imageFileUploadError && (
+              <Alert color="failure">{imageFileUploadError}</Alert>
+            )}
+          </div>
           <div>
             <div className="mb-1 block">
               <Label htmlFor="username" value="Your username" />
