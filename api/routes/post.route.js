822bbf329a1a3a1df711371b7bd9ec99bdcea1f7
trngkhang <ynguyenkhangdev@gmail.com>
2024-04-18 14:09:21 +0700

    feat: create get posts api route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { verifyToken } from "../utils/verifyToken.js";
-import { create } from "../controllers/post.controllers.js";
+import { create, getPosts } from "../controllers/post.controllers.js";
 const router = express.Router();
 
 router.post("/create", verifyToken, create);
+router.get("/posts", getPosts);
 
 export default router;
