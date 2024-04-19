00e612a4d690718bec8b74c60d1c5c6ed1cc8922
trngkhang <ynguyenkhangdev@gmail.com>
2024-04-20 00:10:57 +0700

    feat: create get users api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -2,12 +2,17 @@ import express from "express";
 const router = express.Router();
 import { test } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyToken.js";
-import { updateUser, deleteUser } from "../controllers/user.controller.js";
+import {
+  updateUser,
+  deleteUser,
+  getusers,
+} from "../controllers/user.controller.js";
 
 router.get("/test", test);
 //midleware: verifyToken thuc hien xu ly truoc ham goi router
 //controoler: updateUser thuc hien sau khi goi rourter
 router.put("/update/:userId", verifyToken, updateUser);
 router.delete("/delete/:userId", verifyToken, deleteUser);
+router.get("/getusers", verifyToken, getusers);
 
 export default router;
