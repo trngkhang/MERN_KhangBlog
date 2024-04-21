77d47dba6ce77801daa998e32049d60313d8ad6d
trngkhang <ynguyenkhangdev@gmail.com>
2024-04-21 17:22:23 +0700

    feat: delete user from admin

--- client/src/components/DashUser.jsx
+++ client/src/components/DashUser.jsx
@@ -1,12 +1,15 @@
-import { Table } from "flowbite-react";
+import { Button, Modal, Table } from "flowbite-react";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { FaTimes, FaCheck } from "react-icons/fa";
+import { HiOutlineExclamationCircle } from "react-icons/hi";
 
 export default function DashUser() {
   const { currentUser } = useSelector((state) => state.user);
   const [users, setUsers] = useState([]);
   const [showmore, setShowMore] = useState(true);
+  const [openModal, setOpenModal] = useState(false);
+  const [userIdToDelete, setUserIdToDelete] = useState(null);
 
   useEffect(() => {
     const fetchUsers = async () => {
@@ -43,7 +46,22 @@ export default function DashUser() {
       console.log(error.message);
     }
   };
-
+  const handleDeleteUser = async () => {
+    try {
+      const res = await fetch(`api/user/delete/${userIdToDelete}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+      if (res.ok) {
+        setUsers((prev) => prev.filter((user) => user._id !== userIdToDelete));
+      } else {
+        console.log(data.message);
+      }
+      setOpenModal(false);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <div
       className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100
@@ -87,7 +105,13 @@ export default function DashUser() {
                     )}
                   </Table.Cell>
                   <Table.Cell>
-                    <span className=" font-medium text-red-400 hover:underline cursor-pointer">
+                    <span
+                      onClick={() => {
+                        setOpenModal(true);
+                        setUserIdToDelete(user._id);
+                      }}
+                      className=" font-medium text-red-400 hover:underline cursor-pointer"
+                    >
                       Delete
                     </span>
                   </Table.Cell>
@@ -103,6 +127,30 @@ export default function DashUser() {
               Showmore
             </button>
           )}
+          <Modal
+            show={openModal}
+            size="md"
+            onClose={() => setOpenModal(false)}
+            popup
+          >
+            <Modal.Header />
+            <Modal.Body>
+              <div className="text-center">
+                <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
+                <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+                  Are you sure you want to delete this user?
+                </h3>
+                <div className="flex justify-center gap-4">
+                  <Button color="failure" onClick={handleDeleteUser}>
+                    {"Yes, I'm sure"}
+                  </Button>
+                  <Button color="gray" onClick={() => setOpenModal(false)}>
+                    No, cancel
+                  </Button>
+                </div>
+              </div>
+            </Modal.Body>
+          </Modal>
         </>
       ) : (
         <div className="my-16 flex justify-center">
